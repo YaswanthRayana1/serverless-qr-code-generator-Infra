0a910eed553f052198280707ce2de44809c8d012
Giriraj-sahu-2803 <girirajsahu.201cv218@nitk.edu.in>
2023-10-13 23:55:25 +0530

    initial commit-actual

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
   variable "aws_access_key" {
-    
+    type = string
   }
   variable "aws_secret_key" {
-    
+    type = string
   }
   variable "aws_region" {
-    
+    type = string
   }
   variable "endpoints" {
-    
+    type = string
+  }
+  variable "s3_bucket_name" {
+    type = string
   }
